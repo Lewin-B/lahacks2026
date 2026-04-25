fbf5f86c1fe7a706be3eeb6b5718cba7ec192709
horyzon <kalyan.coliveira@gmail.com>
2026-04-25 18:33:54 -0300

    feat: defines asus ws endpoint on telemetry script (NEEDS CHANGE!)

--- pi-telemetry/thermal_monitor.py
+++ pi-telemetry/thermal_monitor.py
@@ -4,3 +4,8 @@ import os
 from datetime import datetime
 import websockets
 import json
+
+# ASUS server WebSocket endpoint (via Tailscale)
+# I want this to be an env secret actually.
+ASUS_WS_URL = os.getenv("ASUS_WS_URL", "ws://100.x.x.x:5000/ws/telemetry")  # Replace with actual Tailscale IP
+
