4d16eb5a118fffe7e8eae21d9f57d5a23955ac91
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2026-04-24 23:36:43 -0400

    chi init

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello from Go")
+	addr := "localhost:3000"
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	log.Printf("listening on http://%s", addr)
+	err := http.ListenAndServe(addr, r)
+
+	if err != nil {
+		panic(err)
+	}
 }
